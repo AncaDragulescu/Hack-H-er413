2e6c7f8a75d7a0403fc659c4bb92dfb74b9d8280
Becky Mathew <codebecky@gmail.com>
2020-02-08 22:41:58 -0500

    Keeping columns based on exploratory visualizations, float mean imputed

--- model/preprocessing.py
+++ model/preprocessing.py
@@ -13,7 +13,20 @@ diff = lambda l1,l2: [x for x in l1 if x not in l2]
 data = pd.read_csv("C:\\Users\\CodeB\\Documents\\hackher413\\widsdatathon2020\\training_v2.csv")
 df = pd.DataFrame(data)
 
-# columns that have text format:
+# drop columns with bias
+# df.drop("encounter_id", axis=1)
+# df.drop("patient_id", axis=1)
+# df.drop("hospital_id", axis=1)
+# df.drop("icu_id", axis=1)
+# df.drop("gender", axis=1)
+# df.drop("ethnicity". axis=1)
+
+# keep these columns (based on exploratory visualization)
+keep_cols = ["hospital_death", "albumin_apache", "bun_apache", "fio2_apache", "gcs_eyes_apache", "gcs_motor_apache", "gcs_unable_apache", "gcs_verbal_apache", "heart_rate_apache", "intubated_apache", "map_apache", "paco2_apache", "pao2_apache", "ph_apache", "sodium_apache", "temp_apache", "urineoutput_apache", "ventilated_apache", "wbc_apache", "d1_diasbp_invasive_min", "d1_diasbp_min", "d1_diasbp_noninvasive_min", "d1_heartrate_min", "d1_mbp_invasive_min", "d1_mbp_min", "d1_mbp_noninvasive_min", "d1_resprate_max", "d1_spo2_min", "d1_sysbp_invasive_min", "d1_sysbp_min", "d1_sysbp_noninvasive_min", "d1_temp_max", "h1_diasbp_min", "h1_diasbp_noninvasive_max", "h1_heartrate_min", "h1_mbp_min", "h1_resprate_max", "h1_spo2_min", "h1_sysbp_invasive_min", "h1_sysbp_min", "h1_sysbp_noninvasive_min", "h1_temp_max", "d1_albumin_min", "d1_bun_max", "d1_bun_min", "d1_calcium_max", "d1_creatinine_min", "d1_hco3_max", "d1_inr_min", "d1_lactate_min", "d1_platelets_min", "d1_potassium_max", "d1_sodium_max", "d1_wbc_max", "h1_albumin_min", "h1_bilirubin_min", "h1_bun_min", "h1_calcium_min", "h1_creatinine_min", "h1_hco3_max", "h1_hemaglobin_min", "h1_hematocrit_max", "h1_inr_min", "h1_lactate_min", "h1_platelets_min", "h1_potassium_max", "h1_sodium_min", "h1_wbc_max", "d1_arterial_pco2_min", "d1_arterial_ph_min", "d1_arterial_po2_min", "d1_pao2fio2ratio_min", "h1_arterial_pco2_max", "h1_arterial_ph_max", "h1_arterial_po2_max", "h1_pao2fio2ratio_min", "apache_4a_hospital_death_prob", "apache_4a_icu_death_prob", "aids", "cirrhosis", "hepatic_failure", "immunosuppression", "leukemia", "lymphoma", "solid_tumor_with_metastasis"]
+
+df = df[keep_cols]
+
+### text columns ###
 obj_df = df.select_dtypes(include=['object']).copy()
 text_cols = list(obj_df.columns.values)
 
@@ -28,14 +41,14 @@ for header in text_cols:
         df = df.drop(header, axis=1)
         text_cols = diff(text_cols, [header])
     else:
-        # replace text column NA cells with "other"
-        df[header] = df[header].fillna(value="other")
+        # convert to categorical
+        # https://pbpython.com/categorical-encoding.html
+        df[header] = df[header].astype('category')
+        df[header] = df[header].cat.codes
 
-# impute 'categorical' NA cells with mode
-cat_cols = ["hospital_id", "hospital_death", "elective_surgery", \
-"icu_id", "readmission_status", "apache_post_operative", "arf_apache", \
-"gcs_eyes_apache", "gcs_motor_apache", "gcs_unable_apache", "gcs_verbal_apache", \
-"intubated_apache", "ventilated_apache", "aids", "cirrhosis", "diabetes_mellitus", \
+### categorical columns ###
+cat_cols = ["gcs_eyes_apache", "gcs_motor_apache", "gcs_unable_apache", "gcs_verbal_apache", \
+"intubated_apache", "ventilated_apache", "aids", "cirrhosis", \
 "hepatic_failure", "immunosuppression", "leukemia", "lymphoma", \
 "solid_tumor_with_metastasis"]
 for header in cat_cols:
@@ -45,17 +58,11 @@ for header in cat_cols:
         df = df.drop(header, axis=1)
         cat_cols = diff(cat_cols, [header])
     else:
-        # if the percent of NAs is above 10%,
-        # add column stating whether imputation happened (1) or not (0)
-        if percent_na > 0.1:
-            df[header + "_imputed"] = df[header].isna().astype(int)
-        # impute categorical NAs w mode
-        mode_imputer = SimpleImputer(strategy='most_frequent')
-        og = np.array(df[header]).reshape(-1, 1)
-        df[header] = mode_imputer.fit_transform(og)
+        # convert to categorical
+        df[header] = df[header].astype('category')
+        df[header] = df[header].cat.codes
 
-
-# impute continuous NA cells with mean
+### continuous columns ###
 all_cols = list(df.columns.values)
 cont_cols = diff(all_cols, text_cols)
 cont_cols = diff(cont_cols, cat_cols)
@@ -66,19 +73,14 @@ for header in cont_cols:
     if percent_na >= 0.5:
         df = df.drop(header, axis=1)
     else:
-        # if the percent of NAs is above 10%,
-        # add column stating whether imputation happened (1) or not (0)
-        if percent_na > 0.1:
-            df[header + "_imputed"] = df[header].isna().astype(int)
-        # impute categorical NAs w mo
-        mean_imputer = SimpleImputer(strategy='mean')
-        og = np.array(df[header]).reshape(-1, 1)
-        df[header] = mean_imputer.fit_transform(og)
+        # drop rows w NAs
+        # df = df[df[header].notna()]
 
-# text to one hot encoding
-# https://pbpython.com/categorical-encoding.html
-df = pd.get_dummies(df, columns=text_cols, prefix=text_cols)
+        # impute with -1 constant
+        impute = SimpleImputer(strategy='mean')
+        arr = np.array(df[header]).reshape(-1, 1)
+        df[header] = impute.fit_transform(arr)
 
-# column name cleaning
-df.columns = [c.replace(' ', '_').lower() for c in df.columns.values]
-df.to_csv("training_v2_cleaned.csv", index=False)
+print("shape: ", df.shape)
+
+df.to_csv("exploratory_mean.csv", index=False)
